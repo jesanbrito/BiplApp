db548e44e6cd4cdb4e2b0749bfd859e9ddeacd5b
Jesanbrito <jesanbrito@gmail.com>
2020-12-17 19:50:49 -0500

    Inicio

--- app/src/main/java/co/edu/unab/tas/ejuab/biplapp/view/activity/MainActivity.java
+++ app/src/main/java/co/edu/unab/tas/ejuab/biplapp/view/activity/MainActivity.java
@@ -1,22 +1,32 @@
 package co.edu.unab.tas.ejuab.biplapp.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import co.edu.unab.tas.ejuab.biplapp.R;
+import co.edu.unab.tas.ejuab.biplapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final  String TAG_LIFE_CYCLE = "lifeCycle";
+
+    private ActivityMainBinding mainBinding;
+
+
+
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       
+        mainBinding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+
+
     }
 
     @Override
