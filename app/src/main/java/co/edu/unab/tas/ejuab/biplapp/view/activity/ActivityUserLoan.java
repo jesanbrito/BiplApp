f92048bdeb2c0cdea4b6c45de8a0d3c65f74e36b
edna2709 <ednacecilia_1295@hotmail.com>
2020-12-29 22:10:45 -0500

    Filtro Libros

---
+++ app/src/main/java/co/edu/unab/tas/ejuab/biplapp/view/activity/ActivityUserLoan.java
@@ -0,0 +1,16 @@
+package co.edu.unab.tas.ejuab.biplapp.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.edu.unab.tas.ejuab.biplapp.R;
+
+public class ActivityUserLoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_loan);
+    }
+}
